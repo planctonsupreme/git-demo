2a7a8cc30ca123b7b85f9ef1dec68ac57afe139f
oscar <dmob222@gmail.com>
2026-03-30 22:19:29 +0200

    Add power function

--- calculator.py
+++ calculator.py
@@ -12,8 +12,12 @@ def divide(a, b):
         raise ValueError("Cannot divide by zero")
     return a / b
 
+def power(base, exp):
+    return base ** exp
+
 if __name__ == "__main__":
     print(add(10, 5))       # 15
     print(subtract(10, 5))  # 5
     print(multiply(10, 5))  # 50
     print(divide(10, 5))    # 2.0
+    print(power(2, 8))      # 256
